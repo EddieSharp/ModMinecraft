6801137ed6235f1bb0337c3a767a1ee6489fbf98
Somelx <eddierodriguezpastor@gmail.com>
2016-03-09 16:10:22 +0100

    Añadido el item ejemplo.

---
+++ main/java/item/Example.java
@@ -0,0 +1,48 @@
+package item;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class Example extends ItemFood{
+
+	private static Item itemObj;
+	private static String name = "name";
+	
+	static int amount = 1;
+	static float saturation = 1.0f;
+	static boolean isWolfFood = false;
+	
+	private Example() {
+        super(amount, saturation, isWolfFood);
+        this.setUnlocalizedName(name);
+    }
+	
+	
+	public static void preInit() {
+		
+		itemObj = new Example();
+		itemObj.setUnlocalizedName(name);
+		itemObj.setCreativeTab(CreativeTabs.tabFood);
+		GameRegistry.registerItem(itemObj, name);	
+	}
+
+	
+	public static void init() {
+		
+		RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+		ModelResourceLocation mrl = new ModelResourceLocation("somelxmod:" + name, "inventory");
+		renderItem.getItemModelMesher().register(itemObj, 0, mrl);
+		
+	}
+	
+	public static Item getInstance(){
+		
+		return itemObj;
+	}
+
+}
