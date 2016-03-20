b92322c5569653f809cfe617be19740998eed36b
Somelx <eddierodriguezpastor@gmail.com>
2016-03-20 15:15:42 +0100

    modificado el bloque, ahora funciona

--- main/java/block/Bloque.java
+++ main/java/block/Bloque.java
@@ -1,5 +1,8 @@
 package block;
 
+import java.util.Random;
+
+import item.food.Dough;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -9,8 +12,12 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -19,11 +26,17 @@ public class Bloque extends Block {
 	public static Block bloque;
 
 	private final static String name = "Bloque";
+	
+	public Bloque(){
+		super(Material.rock);
+		
+	}
 
 	public static void preInit() {
-		bloque = new Block(Material.rock);
+		bloque = new Bloque();
 		bloque.setUnlocalizedName(name);
 		bloque.setCreativeTab(CreativeTabs.tabMisc);
+		bloque.setHardness(2.0f);
 
 		GameRegistry.registerBlock(bloque, name);
 
@@ -39,22 +52,6 @@ public class Bloque extends Block {
 		
 	}
 	
-	@Override
-	public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state){
-		
-		bloque.dropBlockAsItem(worldIn, pos, state, 2);
-	}
-	
-	
-	
-	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ){
-		
-		//playerIn.addChatComponentMessage(new ITextComponent("Ey"));
-		pos.add(2, 2, 2);
-		playerIn.setFire(3);
-		return false;
-	}
-	
 	public String getName(){
 		
 		return name;
@@ -64,6 +61,35 @@ public class Bloque extends Block {
 		super(materialIn);
 		
 	}
+	
+	@Override
+	 public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+	    {
+	        if (worldIn.isRemote)
+	        {
+	            return true;
+	        }
+	        else
+	        {
+	        	ITextComponent t = new TextComponentString("hola");
+	            playerIn.addChatComponentMessage(t);
+	            //playerIn.openGui(SomelxMod.instance, modGuiId, worldIn, hitX, hitY, hitZ);
+	            //IMerchant im = new IMerchant();
+	            //playerIn.displayVillagerTradeGui();
+	            return true;
+	        }
+	    }
+	
+	
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		// TODO Auto-generated method stub
+		super.getItemDropped(state, rand, fortune);
+		return Dough.getInstance();
+	}
+	
+	
 	
 
 }
--- main/java/somelx/SomelxMod.java
+++ main/java/somelx/SomelxMod.java
@@ -3,6 +3,7 @@ package somelx;
 import net.minecraft.init.Items;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -17,8 +18,13 @@ public class SomelxMod {
     public static final String VERSION = "1.0";
     public static final String MODNAME = "Somelx's mod";
     
+    @Instance(MODID)
+    public static SomelxMod instance;
+    
     @SidedProxy(clientSide="somelx.ClientProxy", serverSide="somelx.ServerProxy")
     public static CommonProxy proxy;
+    
+    
     
     @EventHandler
     public void preinit(FMLPreInitializationEvent event){
